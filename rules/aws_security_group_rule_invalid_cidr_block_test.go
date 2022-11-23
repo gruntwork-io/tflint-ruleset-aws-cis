aef7a29f5ab6137df818e3244683d25aae0d97a6
Marina Limeira <marina@gruntwork.io>
2022-11-23 19:55:39 +0100

    Fix broken syntax

--- rules/aws_security_group_rule_invalid_cidr_block_test.go
+++ rules/aws_security_group_rule_invalid_cidr_block_test.go
@@ -7,16 +7,12 @@ import (
 
 func TestDoesPortRangeContainsRemoteAccess(t *testing.T) {
 
-
-	tests := []struct {
-				Name     string
-				Content  string
-				Expected helper.Issues
-			}{
-				{
-					Name: "issue found",
-					Content: `
-		resource "aws_instance" "web" {
-		   instance_type = "t2.m
-	doesPortRangeContainsRemoteAccess
+	_ := []struct {
+		Name     string
+		Content  string
+		Expected helper.Issues
+	}{
+		{
+			Name:    "issue found",
+			Content: ""}}
 }
